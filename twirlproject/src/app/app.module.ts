6be3ea2ba78487bb24f0f7e6dc4307f954910d01
Charlie Garnaas <cgarnaas555@gmail.com>
2019-01-28 14:49:17 -0600

    nav added

--- twirlproject/src/app/app.module.ts
+++ twirlproject/src/app/app.module.ts
@@ -1,22 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing.module';
+
+
+const appRoutes: Routes = [
+  {
+    path: 'app',
+    component: AppComponent,
+    data: { title: 'Home' }
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    data: { title: 'contact' }
+  },
+  {
+    path: 'portfolio',
+    component: PortfolioComponent,
+    data: { title: 'portfolio' }
+  },
+  {
+    path: '',
+    redirectTo: '/app',
+    pathMatch: 'full' }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     FooterComponent,
-    PortfolioComponent
+    PortfolioComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(
+      [
+        { path: 'contact', component: ContactComponent },
+        { path: 'portfolio', component: PortfolioComponent }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
